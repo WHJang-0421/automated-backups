a733689f4eb3307dd8908434b299ce73b8d99ba1
WoohyukJang <morrranii@gmail.com>
2023-01-27 15:59:52 +0900

    Finish create-backup.sh

---
+++ create-backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo 'The path of the directory/file to create a backup: '
+read original
+
+while [ ! -f $original ] && [ ! -d $original ]
+do
+    echo "The file $original doesn't exist. Please give the path again: "
+    read original
+done
+
+echo 'Compression/archive method (1: zip, 2: tar): '
+read method
+
+while [ "$method" != "1" ] && [ "$method" != "2" ]
+do
+    echo "Wrong input for compression/archive method. Please give either 1 (zip) or 2 (tar) as input. Please give the method again:"
+    read method
+done
+
+if [ $method == "1" ]
+then
+    if [ -f $original ]
+    then
+        zip "./backups/$original.zip" "$original"
+    else
+        zip -r "./backups/$original.zip" "$original"
+    fi
+else
+    tar cf "./backups/$original.tar" "$original"
+fi
+
+echo "Compression/Archive Complete!"
